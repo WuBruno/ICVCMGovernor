baa30fef634a57ed293bfa039b9a3e2173786b68
Bruno Wu <brunowu2000@gmail.com>
2022-06-29 10:10:08 +0100

    Added all environment variables

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -26,7 +26,7 @@ const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
     goerli: {
-      url: process.env.ROPSTEN_URL || "",
+      url: process.env.GOERLI_URL || "",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
