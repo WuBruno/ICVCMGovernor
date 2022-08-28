41bd9ac63f1e784e3afd70fdfe06692d41a5a270
Bruno Wu <brunowu2000@gmail.com>
2022-08-28 19:15:10 +0100

    New integration tests

--- packages/contracts/test/integration/proposals.test.ts
+++ packages/contracts/test/integration/proposals.test.ts
@@ -1,7 +1,7 @@
 import { mine } from "@nomicfoundation/hardhat-network-helpers";
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
 import { Roles } from "~/@types";
 import { deployContracts } from "~/services/deployment";
 import {
@@ -52,7 +52,7 @@ describe("1 Proposal Integration Tests", async () => {
   });
 
   describe("1.1 Passing Types of Proposals", () => {
-    it("1.1.1 should pass a strategy proposal", async () => {
+    it("1.1.1 should pass an strategy proposal", async () => {
       const newStrategy = "The best strategy";
       const setProposalCall = constitution.interface.encodeFunctionData(
         "addStrategy",
@@ -73,7 +73,84 @@ describe("1 Proposal Integration Tests", async () => {
       expect(await constitution.getStrategy(1)).to.equal(newStrategy);
     });
 
-    it("1.1.2 should pass a principle proposal", async () => {
+    it("1.1.2 should pass an update strategy proposal", async () => {
+      const newStrategy = "The best strategy";
+      const setProposalCall = constitution.interface.encodeFunctionData(
+        "addStrategy",
+        [newStrategy]
+      );
+
+      const description = "New strategy proposal";
+
+      await createAndPassProposal(
+        governor.connect(secretariat),
+        constitution.address,
+        setProposalCall,
+        description,
+        [owner, user2],
+        regulator
+      );
+
+      expect(await constitution.getStrategy(1)).to.equal(newStrategy);
+
+      const updateStrategy = "Updated strategy";
+      const setProposalCall2 = constitution.interface.encodeFunctionData(
+        "updateStrategy",
+        [1, updateStrategy]
+      );
+
+      await createAndPassProposal(
+        governor.connect(secretariat),
+        constitution.address,
+        setProposalCall2,
+        description,
+        [owner, user2],
+        regulator
+      );
+
+      expect(await constitution.getStrategy(1)).to.equal(updateStrategy);
+    });
+
+    it("1.1.3 should pass an remove strategy proposal", async () => {
+      const newStrategy = "The best strategy";
+      const setProposalCall = constitution.interface.encodeFunctionData(
+        "addStrategy",
+        [newStrategy]
+      );
+
+      const description = "New strategy proposal";
+
+      await createAndPassProposal(
+        governor.connect(secretariat),
+        constitution.address,
+        setProposalCall,
+        description,
+        [owner, user2],
+        regulator
+      );
+
+      expect(await constitution.getStrategy(1)).to.equal(newStrategy);
+
+      const removeStrategyCall = constitution.interface.encodeFunctionData(
+        "removeStrategy",
+        [1]
+      );
+
+      await createAndPassProposal(
+        governor.connect(secretariat),
+        constitution.address,
+        removeStrategyCall,
+        description,
+        [owner, user2],
+        regulator
+      );
+
+      expect(constitution.getStrategy(1)).to.be.revertedWith(
+        "Invalid Strategy Id"
+      );
+    });
+
+    it("1.1.4 should pass a add principle proposal", async () => {
       const newPrinciple = "The best principle 2";
       const setPrincipleCall = constitution.interface.encodeFunctionData(
         "addPrinciple",
@@ -94,7 +171,84 @@ describe("1 Proposal Integration Tests", async () => {
       expect(await constitution.getPrinciple(1)).to.equal(newPrinciple);
     });
 
-    it("1.1.3 should pass adding director proposal", async () => {
+    it("1.1.5 should pass a update principle proposal", async () => {
+      const newPrinciple = "The best principle 2";
+      const setPrincipleCall = constitution.interface.encodeFunctionData(
+        "addPrinciple",
+        [newPrinciple]
+      );
+
+      const description = "New principle proposal";
+
+      await createAndPassProposal(
+        governor.connect(expert),
+        constitution.address,
+        setPrincipleCall,
+        description,
+        [owner, user2],
+        regulator
+      );
+
+      expect(await constitution.getPrinciple(1)).to.equal(newPrinciple);
+
+      const updatePrinciple = "Update Principle";
+      const setProposalCall2 = constitution.interface.encodeFunctionData(
+        "updatePrinciple",
+        [1, updatePrinciple]
+      );
+
+      await createAndPassProposal(
+        governor.connect(expert),
+        constitution.address,
+        setProposalCall2,
+        description,
+        [owner, user2],
+        regulator
+      );
+
+      expect(await constitution.getPrinciple(1)).to.equal(updatePrinciple);
+    });
+
+    it("1.1.6 should pass a remove principle proposal", async () => {
+      const newPrinciple = "The best principle 2";
+      const setPrincipleCall = constitution.interface.encodeFunctionData(
+        "addPrinciple",
+        [newPrinciple]
+      );
+
+      const description = "New principle proposal";
+
+      await createAndPassProposal(
+        governor.connect(expert),
+        constitution.address,
+        setPrincipleCall,
+        description,
+        [owner, user2],
+        regulator
+      );
+
+      expect(await constitution.getPrinciple(1)).to.equal(newPrinciple);
+
+      const removePrincipleCall = constitution.interface.encodeFunctionData(
+        "removePrinciple",
+        [1]
+      );
+
+      await createAndPassProposal(
+        governor.connect(expert),
+        constitution.address,
+        removePrincipleCall,
+        description,
+        [owner, user2],
+        regulator
+      );
+
+      expect(constitution.getPrinciple(1)).to.be.revertedWith(
+        "Invalid Strategy Id"
+      );
+    });
+
+    it("1.1.7 should pass adding director proposal", async () => {
       const description = "Adding new director";
       const name = ethers.utils.formatBytes32String("Director");
       const addMemberCall = roles.interface.encodeFunctionData("addMember", [
@@ -122,7 +276,7 @@ describe("1 Proposal Integration Tests", async () => {
       ).to.equal(1);
     });
 
-    it("1.1.4 should reject proposal when adding the member to the same role", async () => {
+    it("1.1.8 should reject proposal when adding the member to the same role", async () => {
       const description = "Adding new director";
       const name = ethers.utils.formatBytes32String("Director");
       const addMemberCall = roles.interface.encodeFunctionData("addMember", [
@@ -143,7 +297,7 @@ describe("1 Proposal Integration Tests", async () => {
       ).to.revertedWith("Member already exists");
     });
 
-    it("1.1.5 should pass remove member proposal", async () => {
+    it("1.1.9 should pass remove member proposal", async () => {
       const description = "Remove director";
       const removeMemberCall = roles.interface.encodeFunctionData(
         "removeMember",
@@ -169,7 +323,7 @@ describe("1 Proposal Integration Tests", async () => {
       ).to.equal(0);
     });
 
-    it("1.1.6 should pass set voting quorum proposal", async () => {
+    it("1.1.10 should pass set voting quorum proposal", async () => {
       const description = "Change Quorum";
       const newQuorum = 66;
       const updateQuorumCall = governor.interface.encodeFunctionData(
@@ -192,7 +346,7 @@ describe("1 Proposal Integration Tests", async () => {
       ).to.equal(newQuorum);
     });
 
-    it("1.1.7 should pass set voting period proposal", async () => {
+    it("1.1.11 should pass set voting period proposal", async () => {
       const description = "Change Voting Period";
       const newVotingPeriod = 100;
       const updateQuorumCall = governor.interface.encodeFunctionData(
@@ -213,6 +367,80 @@ describe("1 Proposal Integration Tests", async () => {
         await governor.votingPeriod(),
         "Voting period should have updated"
       ).to.equal(newVotingPeriod);
+    });
+
+    it("1.1.12 should pass add proposal authorization proposal", async () => {
+      const description = "Add Proposal Authorization Proposal";
+      const functionCall = roles.interface.encodeFunctionData(
+        "addProposalAuthorization",
+        [
+          roles.address,
+          roles.interface.getSighash("addMember"),
+          Roles.Secretariat,
+        ]
+      );
+
+      await createAndPassProposal(
+        governor.connect(secretariat),
+        roles.address,
+        functionCall,
+        description,
+        [owner, user2],
+        regulator
+      );
+
+      expect(
+        await roles.hasProposalAuthorization(
+          roles.address,
+          roles.interface.getSighash("addMember"),
+          Roles.Secretariat
+        )
+      ).to.equal(true);
+    });
+
+    it("1.1.13 should pass remove proposal authorization proposal", async () => {
+      const description = "Add Proposal Authorization Proposal";
+      const functionCall = roles.interface.encodeFunctionData(
+        "removeProposalAuthorization",
+        [roles.address, roles.interface.getSighash("addMember"), Roles.Director]
+      );
+
+      await createAndPassProposal(
+        governor.connect(secretariat),
+        roles.address,
+        functionCall,
+        description,
+        [owner, user2],
+        regulator
+      );
+
+      expect(
+        await roles.hasProposalAuthorization(
+          roles.address,
+          roles.interface.getSighash("addMember"),
+          Roles.Director
+        )
+      ).to.equal(false);
+    });
+
+    it("1.1.14 should pass upgrade contract proposal", async () => {
+      const Contract = await ethers.getContractFactory("ICVCMGovernor");
+      const impl = await upgrades.deployImplementation(Contract);
+
+      const call = governor.interface.encodeFunctionData("upgradeTo", [
+        impl.toString(),
+      ]);
+
+      await createAndPassProposal(
+        governor.connect(secretariat),
+        governor.address,
+        call,
+        "upgradeTo",
+        [owner, user2],
+        regulator
+      );
+
+      expect(await governor.getVersion()).to.equal(2);
     });
   });
 });
